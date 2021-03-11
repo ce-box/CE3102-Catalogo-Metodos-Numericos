1ccbd6998b5806954e85ec7db8cd3e3dce74609f
Martin Calderón <38304056+gaburolo@users.noreply.github.com>
2021-03-10 21:03:54 -0600

    Biseccion comentado y muller V1

--- Catalogo/Octave/archivo_biseccion.m
+++ Catalogo/Octave/archivo_biseccion.m
@@ -5,7 +5,7 @@ function archivo_biseccion
   f='exp(x)-x-2';
   %f='exp(-x)-x'; %a=0 b=1 tol=1*10^-3;
   a=0;
-  b=2;
+  b=0;
   tol=10^-4;
   iterMax=100;
   %P2: Llamar a la función
@@ -19,27 +19,34 @@ function[xk,error]=biseccion(f,a,b,iterMax,tol)
   f=matlabFunction(f1);%Función f en formato del lenguaje M
   k=1;%Se define iteracion inicial
   e=[];%Lista que contendra los valores de error
-  for i=1:iterMax
-    xk=(a+b)/2; %Calculo y actualizacion de la aproximacion
-    fx=f(xk);%Valor de la funcion con respecto a la aproximacion
-    error=(b-a)/2^k;%Se calcula el error
-    display(['---------------']);
-    display(['iteracion #' num2str(k)]);
-    display(['a:' num2str(a)]);
-    display(['b:' num2str(b)]);
-    e=[e error];%Se agrega a la lista el valor de error actual
-    k=k+1;%Actualiza iteraciones
-    if f(a)*fx<0 %Se verifica la condicion de f(a)*f(x) sea menor a 0  
-       b=xk;%Se actualiza el valor de b
-    elseif fx*f(b)<0%Sino verifica la condicion de f(b)*f(x) sea menor a 0  
-       a=xk;%Se actualiza el valor de b
-    end
-    if abs(error)<tol%Condicion de parada
-      break;
-    end
-  endfor
-  plot(2:k,e);%Se muestra la grafica de ERROR VS Iteraciones de la funcion
-  title ("Biseccion error vs iteraciones");
-  xlabel("Iteraciones")
-  ylabel("Error")
+  if f(a)*f(b)<=0
+    for i=1:iterMax
+      xk=(a+b)/2; %Calculo y actualizacion de la aproximacion
+      fx=f(xk);%Valor de la funcion con respecto a la aproximacion
+      error=(b-a)/2^k;%Se calcula el error
+      display(['---------------']);
+      display(['iteracion #' num2str(k)]);
+      display(['a:' num2str(a)]);
+      display(['b:' num2str(b)]);
+      e=[e error];%Se agrega a la lista el valor de error actual
+      k=k+1;%Actualiza iteraciones
+      if f(a)*fx<0 %Se verifica la condicion de f(a)*f(x) sea menor a 0  
+         b=xk;%Se actualiza el valor de b
+      elseif fx*f(b)<0%Sino verifica la condicion de f(b)*f(x) sea menor a 0  
+         a=xk;%Se actualiza el valor de b
+      end
+      if abs(error)<tol%Condicion de parada
+        break;
+      end
+    endfor
+    plot(2:k,e);%Se muestra la grafica de ERROR VS Iteraciones de la funcion
+    title ("Biseccion error vs iteraciones");
+    xlabel("Iteraciones");
+    ylabel("Error");
+  else%En caso de no cumplir el teorema de bolzano
+    xk=0;%Se coloca en valor por defecto por el return de la funcion
+    error=0;
+    display(['No cumple el teorema de bolzano']);%Muestra mensaje al no cumplir el teorema
+  end
+  
 end
---
+++ Catalogo/Octave/archivo_muller.m
@@ -0,0 +1,93 @@
+function archivo_muller
+  %Ejemplo Numérico
+  %P1: Definir parámetros de entrada
+  f='sin(x)-(x/2)';
+  x0=2;
+  x1=2.2;
+  x2=1.8;;
+  iterMax=2;
+  
+  muller(f,x0,x1,x2,4);
+
+end
+
+
+function muller(f,x0,x1,x2,iterMax)
+  pkg load symbolic
+  syms x
+  f1=sym(f);%convierte el Texto en  simbolico
+  f=matlabFunction(f1);
+  while(iterMax>0)
+    lista1=[]
+    val=[];
+    a=[];
+    b=[];
+    c=[0,0,1];
+    a=[a ((x0-x2)^2)];
+    a=[a ((x0-x2))];
+    a=[a 1];
+  
+    b=[b ((x1-x2)^2)];
+    b=[b ((x1-x2))];
+    b=[b 1];
+  
+    lista1=[lista1;a];
+    lista1=[lista1;b];
+    lista1=[lista1;c];
+  
+    val=[val;f(x0)]
+    val=[val;f(x1)]
+    val=[val;f(x2)]
+    
+    r=resolver(lista1,val,x0,x1,x2)
+    [t0,t1,t2]=masCercanos(x0,x1,x2,r)
+    x0=t0;
+    x1=t1;
+    x2=t2;
+
+    iterMax=iterMax-1;
+    clc;
+  end
+    display(['-------------'])
+    display(['la aproximacion R=' num2str(r)]) 
+end
+function [r]=resolver(matr,val,x0,x1,x2)
+  resultado=[];
+  resultado=inv(matr)*val;
+  r=obtenerR(resultado(1),resultado(2),resultado(3),x2)
+  
+end
+
+function [signo]=obtenerSig(b)
+  signo=0;
+  if b<0
+    signo=-1;
+  else
+    signo=1;
+  end
+end
+
+function [r]=obtenerR(a,b,c,x2)
+  signo=obtenerSig(b)
+  r=x2-((2*c)/(b+signo*sqrt((b^2)-4*a*c)))
+end
+
+function [t0,t1,t2]=masCercanos(x0,x1,x2,r)
+  m0=abs(r-x0);
+  m1=abs(r-x1);
+  m2=abs(r-x2);
+  if(m0<m2 && m1<m2)
+    t0=x0;
+    t1=x1;
+    t2=r;
+  elseif(m0<m1 && m2<m1)
+    t0=x0;
+    t1=x2;
+    t2=r;
+  elseif(m1<m0 && m2<m0)
+    t0=x1;
+    t1=x2;
+    t2=r;
+  end
+end
+  
