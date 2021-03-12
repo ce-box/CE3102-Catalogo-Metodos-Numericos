1815215a3c264bdb5789138621fc963558e2a65a
esteban alvarado <estalvgs1999@gmail.com>
2021-03-12 00:32:02 -0600

    [add][fix] Se implementa el método de la falsa posición y se arreglan problemas relacionados con las condiciones de parada.

--- Catalogo/C++/falsa_posicion.cpp
+++ Catalogo/C++/falsa_posicion.cpp
@@ -11,13 +11,13 @@ using namespace std;
  * @return Retorna el resultado de evaluar f(x).
  */ 
 double f(double x){
-    // f(x) = cos²(2x) - x²
-    return pow(cos(2*x), 2) - pow(x,2); 
+    // f(x) = cos(x)
+    return cos(x)- x; 
 }
 
 
 /**
- * @brief Calcula el siguiente valor de la sucesión.
+ * @brief Calcula el siguiente valor de la sucesión mediante la fórmula de la secante.
  * @param x_k Valor actual de la sucesión.
  * @param x_prev Valor anterior de la sucesión x_(k-1).
  * @return Retorna el siguiente valor de la sucesión x_(k+1).
@@ -54,28 +54,46 @@ bool condicion_parada(double x_k, double x_prev, double tol){
     else return false;
 }
 
+/**
+ * 
+ */
+bool teorema_bolzano(double a, double b){
+    return f(a)*f(b) < 0;
+}
+
 
 /**
  * @brief Permite encontrar los ceros de una función f(x) de forma iterativa
- * aplicando el método de la secante.
- * @param x_0 Valor de la condición inicial x_0.
- * @param x_1 Valor de la condición inicial x_1.
+ * aplicando el método de la falsa posición.
+ * @param a Valor de inicio del intervalo.
+ * @param x_1 Valor final del intervalo.
  * @param tol Valor de la tolerancia de resultado aceptable.
  * @param max_itr Cantidad máxima de iteraciones que se pueden realizar.
  * @return Retorna el valor de la aproximación y un vector con los errores
  * obtenidos en cada iteración.
  */ 
-tuple<double, vector<double>> secante(double x_0, double x_1, double tol, int max_itr=100){
+tuple<double, vector<double>> falsa_posicion(double a, double b, double tol, int max_itr=100){
+    // Primero se verifica el teorema de Bolzano
+    if (!teorema_bolzano(a,b)){
+        throw "[Error 001] La función no cumple con el teorema de Bolzano en el intervalo dado.";
+    }
 
+    cout << max_itr << endl;
     vector<double> error;
-    double x_k = x_1, x_prev = x_0, x_sgt;
-    int k = 1;
+    double x_k;
+    int k = 2;
 
-    while (!condicion_parada(x_k, x_prev, tol) && k < max_itr){
-        x_sgt = calcular_sgte_valor(x_k, x_prev);   // Calcula el valor para x_(k+1).
-        x_prev = x_k;                               // Asigna a x_(k-1) = x_k.
-        x_k = x_sgt;                                // Asigna a x_k = x_(k+1).
-        error.push_back(calcular_error(x_k));       // Agrega el error al vector.
+    while(!condicion_parada(b,a,tol) && k < max_itr){
+        cout << k << endl;
+        x_k = calcular_sgte_valor(b,a);
+        error.push_back(calcular_error(x_k));
+
+        if (teorema_bolzano(a, x_k)) b = x_k;
+        else if (teorema_bolzano(x_k, b)) a = x_k;
+        else {
+            cout << "[Error 002] No es posible continuar calculando. No se cumple el teorema de Bolzano." << endl;
+            break;
+        }
         k++;
     }
 
@@ -83,13 +101,9 @@ tuple<double, vector<double>> secante(double x_0, double x_1, double tol, int ma
 }
 
 
-// TODO: Implementar la función que grafique el error
-void plot(){}
-
-
 int main(int argc, char const *argv[])
 {
-    tuple<double, vector<double>> r = secante(0.75, 0.5, 10e-8,4);
+    tuple<double, vector<double>> r = falsa_posicion(0.5, 0.7854, 10e-3, 5);
 
     double x = get<0>(r);
     vector<double> error_set = get<1>(r);
@@ -99,5 +113,11 @@ int main(int argc, char const *argv[])
     cout << "aprox:: " << x << endl;
     cout << "k:: " << k << endl;
 
+    for(int i = 0; i < k; i++){
+        cout << error_set.at(i) << endl;
+    }
+
     return 0;
 }
+
+// TODO: Función de plot
