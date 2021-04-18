9577dc15898576f6737b04aabbc8dee5eaad068b
Esteban Alvarado <estalvgs1999@gmail.com>
2021-04-18 16:24:34 -0600

    [add][cholesky] Estructura basica de la solucion.

---
+++ Catalogo/03. Sistemas de Ecuaciones/C++/fact_cholesky.cpp
@@ -0,0 +1,25 @@
+/* ------------------------------------------------------------
+ * @file: fact_cholesky.cpp
+ * @dependencias: None
+ * @version 3.0
+ * ------------------------------------------------------------*/
+
+// Compile and run:
+// [1] $ g++ fact_cholesky.cpp -o fc.out -std=c++11 
+// [2] $ ./fc.out
+
+using namespace std;
+
+const int n = 3;
+double A[n][n];
+
+int fact_cholesky(double coefs[n][n], double ind_terms[n]){
+    return 0;
+}
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
