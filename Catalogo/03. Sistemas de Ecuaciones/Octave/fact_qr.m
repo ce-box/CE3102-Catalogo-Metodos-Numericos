a531f66f8aabc885e9ffaec12fefc3b08c2fd8f1
Agustín V <joseagustinveneveg@gmail.com>
2021-04-29 21:25:16 -0600

    [UPDATE] Factorizacion QR
    logic fix

--- Catalogo/03. Sistemas de Ecuaciones/Octave/fact_qr.m
+++ Catalogo/03. Sistemas de Ecuaciones/Octave/fact_qr.m
@@ -3,32 +3,16 @@ function archivo_qr
   %Valor de la matriz A
   A = [12 -51 4; 6 167 -68; -4 24 -41]
        
-  %Valor de la tolerancia
-  tol = 10 ^ -10
+  %vector de terminos independientes
+  term_ind = []
   
   %Se llama a la funcion fact_qr con A y tol
-  [x] = fact_qr(A, tol)
+  [x] = fact_qr(A, term_ind)
   
 end
 
-function [x] = fact_qr(A, tol)
+function [x] = fact_qr(A, ind)
   
-  len_A = size(A) %Se guarda el tamano de A
-  last_A = zeros(len_A) %Se crea el valor anterior de A 
-
-  while 1
-    
-      [Q, R] = qr(A) 
-      last_A = A % Reasigna el ultimo A calculado
-      A = R * Q %Reasigna A actual
-      norm1 = norm(last_A - A) %Calcula la norma
-      
-      %Condicion de parada
-      if norm1 < tol
-          break;
-      end
-  end
   
-  x = A
   
 end
