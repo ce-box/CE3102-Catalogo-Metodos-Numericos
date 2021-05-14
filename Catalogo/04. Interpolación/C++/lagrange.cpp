9941aa723ed7e912d873f645f6a5c41b8c06972e
esteban alvarado <estalvgs1999@gmail.com>
2021-05-14 09:28:40 -0600

    [ok] Metodo de Lagrange terminado y probado con éxito.

--- Catalogo/04. Interpolación/C++/lagrange.cpp
+++ Catalogo/04. Interpolación/C++/lagrange.cpp
@@ -4,7 +4,7 @@
  * @version 0.1
  * ------------------------------------------------------------*/
 
-// [1] $ g++ lagrange.cpp -o lg.out -std=c++11 -I/usr/include/python3.8 -lpython3.8 
+// [1] $ g++ lagrange.cpp -o lg.out -std=c++11 -I/usr/include/python3.8 -lpython3.8 -lginac -lcln
 // [2] $ ./lg.out
 
 #include <iostream>
@@ -14,6 +14,32 @@
 
 using namespace std;
 using namespace GiNaC;
+
+
+/**
+ * @brief 
+ * 
+ * @param xv 
+ * @param k 
+ * @return ex 
+ */
+ex lagrange_basis(vector<double> xv, int k){
+    
+    symbol x("x");
+    int n = xv.size();
+    ex Lk = 1;
+    //ex xi,xk;
+
+    for (int i = 0; i < n; i++){
+        if (i != k){
+            //xi = xv[i];
+            //xk = xv[k];
+            Lk *= (x - xv[i])/(xv[k] - xv[i]);
+        }
+    }
+
+    return Lk;
+}
 
 
 /**
@@ -30,38 +56,20 @@ ex lagrange(vector<double> xv, vector<double> yv){
     ex poly = 0;
 
     for (int k = 0; k < n; k++){
+        //ex y = yv[k];
         poly += yv[k] * lagrange_basis(xv,k);
     }
 
-    return poly;
-}
-
-
-/**
- * @brief 
- * 
- * @param xv 
- * @param k 
- * @return ex 
- */
-ex lagrange_basis(vector<double> xv, int k){
-    
-    symbol x("x");
-    int n = xv.size();
-    ex Lk = 1;
-
-    for (int i = 0; i < n; i++){
-        if (i != k){
-            Lk *= (x - xv[i])/(xv[k] - xv[i]);
-        }
-    }
-
-    return Lk;
+    return poly.normal();
 }
 
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    vector<double> xv{-2, 0, 1};
+    vector<double> yv{0, 1, -1};
+    symbol x("x");
+    ex p = lagrange(xv,yv);
+    cout << "P(x) = " << p << endl; 
     return 0;
 }
