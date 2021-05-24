799c6b5708e67dcc8a328f43845ed6c790e2ff6f
Agustín V <joseagustinveneveg@gmail.com>
2021-05-24 12:47:33 -0600

    [UPDATE] Metodo Simpson Compuesto, se añadieron comentarios al codigo

--- Catalogo/05. Integración Numérica/Python/simpson_compuesto.py
+++ Catalogo/05. Integración Numérica/Python/simpson_compuesto.py
@@ -13,45 +13,45 @@ def simpson_compuesto(func, interv):
     d4f = d3f.diff(x) #Se calcula la cuarta derivada de f
     d4fx = lambdify(x, d4f, modules=['numpy']) #Se inicializa la función de la cuarta derivada de f
 
-    a = interv[0]
-    b = interv[-1]
-    m = 7
+    a = interv[0] #Se extrae el valor inicial del intervalo
+    b = interv[-1] #Se extrae el valor final del intervalo
+    m = 7 #Cantidad de puntos
 
-    h = (b-a)/(m-1)
-    x = []
+    h = (b-a)/(m-1) #Se calcula el valor de "h"
+    x = [] #Se inicializa la lista de los valores de x (x0, x1, x2, ...)
     
-    cont = 0
+    cont = 0 #Inicializa contador para bucle
 
     while(cont < m):
         
-        xk = a+cont*h
-        x.append(xk)
-        cont += 1
+        xk = a+cont*h #Se calcula xk
+        x.append(xk) #Se añade xk a la lista de valores de x
+        cont += 1 #Aumenta contador
 
-    cont = 1
-    sumPar = 0
-    sumImp = 0
+    cont = 1 #Inicializa contador para bucle (en 1 porque no contempla x0)
+    sumPar = 0 #Inicializa resultado de sumatoria de indices pares
+    sumImp = 0 #Inicializa resultado de sumatoria de indices impares
             
-    while(cont < (len(x) - 1)):
+    while(cont < (len(x) - 1)): #Punto de parada "len(x) - 1" porque no contempla xn
 
-        if(par(cont)):
-            sumPar += fx(x[cont])
-        else:
-            sumImp += fx(x[cont])
-        cont += 1
+        if(par(cont)): #Verifica indice par
+            sumPar += fx(x[cont]) #Sumatoria indices pares
+        else: #Si el indice no es par
+            sumImp += fx(x[cont]) #Sumatoria indices impares
+        cont += 1 #Incrementa contador
 
-    aprox = (h/3)*(fx(x[0]) + 2*sumPar + 4*sumImp + fx(x[-1]))
-    error = (((b - a)*(h**4))/180) * abs(d4fx(2))
+    aprox = (h/3)*(fx(x[0]) + 2*sumPar + 4*sumImp + fx(x[-1])) #Calcula aproximacion
+    error = (((b - a)*(h**4))/180) * abs(d4fx(2)) #Calcula error
 
-    print([aprox, error])
-    return[aprox, error]
+    print([aprox, error]) #Muestra en pantalla la aproximacion y el error
+    return[aprox, error] #Retorna aproximacion y error
     
 
-def par(x):
-    if((x%2) == 0):
-        return True
-    else:
-        return False
+def par(indice): #Verifica paridad de indice
+    if((indice%2) == 0): #Si la division entre 2 da cero (es par)
+        return True #True si es par
+    else: #Si no da cero (es impar)
+        return False #False si es impar
 
-simpson_compuesto('log(x)', [2,5])
+simpson_compuesto('log(x)', [2,5]) #Ejemplo
             
