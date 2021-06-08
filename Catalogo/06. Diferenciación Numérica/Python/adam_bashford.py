4a22b016ad7bb230487544c25d719b52c6fade2d
Agustín V <joseagustinveneveg@gmail.com>
2021-06-08 11:38:27 -0600

    [UPDATE] Adición de comentarios al método de Adam-Bashford de orden 4

--- Catalogo/06. Diferenciación Numérica/Python/adam_bashford.py
+++ Catalogo/06. Diferenciación Numérica/Python/adam_bashford.py
@@ -12,44 +12,49 @@ def adam_bashford_4(func, interv, h):
     f = sympify(func) #Se traduce el string "func" a una función de sympy 
     fx = lambdify([x, y], f, modules=['numpy']) #Se inicializa la función f(x,y) 
 
-    listaX = [2, 2.2, 2.4]
-    listaY = [1, 1.191, 1.365]
+    listaX = [2, 2.2, 2.4] #Valores iniciales x
+    listaY = [1, 1.191, 1.365] #Valores inicial y
 
-    x_temp = 2.6
-    y_temp = 1.528
+    x_temp = 2.6 #Valor temporal xk
+    y_temp = 1.528 #Valor temporal yk
 
-    cont = 3
+    cont = 3 #Contador para yk
 
-    while(x_temp <= interv[1]):      
+    while(x_temp <= interv[1]): #Mientras se encuentre en el dominio [2,4] 
 
 
-        listaX.append(float(x_temp))
-        listaY.append(float(y_temp))
+        listaX.append(float(x_temp)) #Añade a lista xk
+        listaY.append(float(y_temp)) #Añade a lista yk
 
-        y_temp = yk(listaX, listaY, h, cont, fx)
-        x_temp += h
+        y_temp = yk(listaX, listaY, h, cont, fx) #Llama función que calcula yk
+        x_temp += h #Incrementa valor de x en h
 
-        cont += 1
+        cont += 1 #Incrementa contador
 
-    p = lagrange(listaX,listaY)
-    print("Polinomio de interpolación: ", p)
-    x = np.linspace(2,4,40)
-    plt.scatter(x, p(x),color='r',zorder=1)
+    p = lagrange(listaX,listaY) #Crea polinomio de interpolación
+    print("Polinomio de interpolación: \n", p) #Imprime polinomio de interpolación
+    x = np.linspace(2,4,40) #Crea una lista con n valores entre el dominio [2,4]
+
+    #Formato de la línea y los puntos
+    plt.scatter(x, p(x),color='r',zorder=1) 
     plt.plot(x, p(x),color='b',zorder=2)
 
+    #Especifiaciones de gráfica
     plt.title("Gráfica polinomio de interpolación")
     plt.xlabel("x")
     plt.ylabel("f(x)")
     plt.ylim([-5,5])
-    
+
+    #Muestra gráfica
     plt.show()
     
     return [listaX, listaY]
 
-def yk(listaX, listaY, h, cont, fx):
+def yk(listaX, listaY, h, cont, fx): 
 
+    #Función que crea yk
     yk = listaY[cont] + (h/24) * (55 * fx(listaX[cont], listaY[cont]) - 59 * fx(listaX[cont-1], listaY[cont-1]) + 37 * fx(listaX[cont-2], listaY[cont-2]) - 9 * fx(listaX[cont-3], listaY[cont-3]))
 
     return yk
 
-resultado = adam_bashford_4('1 + (x - y) ** 2', [2,4], 0.2)
+adam_bashford_4('1 + (x - y) ** 2', [2,4], 0.2)
