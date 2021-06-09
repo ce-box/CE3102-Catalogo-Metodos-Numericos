f9b70a719286a5aea9d826b0941d5e5d9c0514e4
Olman Castro Hernández <olcastro@estudiantec.cr>
2021-06-09 14:48:47 -0600

    [Update] Documentación Predictor Corrector

--- Catalogo/06. Diferenciación Numérica/Python/predictor_corrector.py
+++ Catalogo/06. Diferenciación Numérica/Python/predictor_corrector.py
@@ -4,6 +4,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sympy import *
 
+#Parametros de entrada
+#func: funcion de dos variables, y0: valor inicial, interv: interbalo del funcion
+# N_puntos: cantidad de puntos 
+#Salida
+#resultado: polinomio de interpolacion
+#Funcion Predictor corrector
 def predictor_corrector(func, y0, interv, N_puntos):
 
     x  = Symbol('x') #Inicializa "x" como símbolo
@@ -11,12 +17,15 @@ def predictor_corrector(func, y0, interv, N_puntos):
     f  = sympify(func) #Se traduce el string "func" a una función de sympy 
     fxy = lambdify([x, y], f, modules=['numpy']) #Se inicializa la función f(x,y) 
     
+    #Variables a  y b
     a = interv[0]
     b = interv[1]
 
+    #Listas de puntos
     listaX = [] 
     listaY = []
 
+    #Valor de h, X0, Y0, n
     h = (b-a)/(N_puntos-1)
     xn = 0
     yn = y0
@@ -25,17 +34,20 @@ def predictor_corrector(func, y0, interv, N_puntos):
     listaX.append(xn)
     listaY.append(yn)
 
+    #Clico principal de ejecucion 
     while (xn < b):
+        #Calculo de yn+1 con Euler
         yn_euler = euler(fxy, yn, xn, h)
 
-        n += 1
-        xn_sig = a + n * h
+        n += 1 #n siguiente
+        xn_sig = a + n * h #xn+1
 
+        #Calculo de yn+1 predictor corrector
         yn = yn + h * (fxy(xn, yn) + fxy(xn_sig, yn_euler))/2
         xn = xn_sig
 
-        listaX.append(round(xn, 2))
-        listaY.append(round(yn, 4))
+        listaX.append(round(xn, 2)) #Punto a la lista
+        listaY.append(round(yn, 4)) #Punto a la lista
 
     p = lagrange(listaX,listaY) #Crea polinomio de interpolación
     print("Polinomio de interpolación: \n", p) #Imprime polinomio de interpolación
@@ -54,12 +66,20 @@ def predictor_corrector(func, y0, interv, N_puntos):
     #Muestra gráfica
     plt.show()
     
-    return [listaX, listaY]
+    #Resultado
+    return p
 
+#Parametros de entrada
+#fxy: funcion de dos variables, yn: valor inicial, xn: valor inicial, h: valor h
+#Salida
+#resultado: yn+1 por euler
+#Funcion Euler
 def euler(fxy, yn, xn, h):
-
+    
+    #Calculo yn+1 con euler
     yn_euler = yn + h * fxy(xn,yn)
 
+    #Resultado
     return yn_euler 
 
 
